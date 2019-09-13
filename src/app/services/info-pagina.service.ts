926eeedb390ae636eadc7539782e427220ea1feb
Kleiton Cambronero <1030klei@gmail.com>
2019-09-13 14:41:30 -0600

    Fin seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,16 +9,32 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any [] = [];
 
   constructor( private http: HttpClient ) {
 
     // Leer el archivo JSON
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo(){
+
     this.http.get('assets/data/data-pagina.json')
     .subscribe( (resp: InfoPagina) => {
       this.cargada = true;
       this.info = resp;
-      console.log(resp);
     });
 
   }
+
+  private cargarEquipo(){
+
+    this.http.get('https://angular-html-64aaa.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
+      this.equipo = resp;
+      // console.log(resp);
+    });
+  }
+
 }
